df063fa3363c2996ef49b1b6a4c04ea516aabce8
AlynXZheng <136864549+AlynXZheng@users.noreply.github.com>
2023-07-04 01:11:10 -0600

    Update [orderId].js

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -78,6 +78,7 @@ export const getServerSideProps = async (context) => {
   return { props: { order: data, currentUser: currentUser.data } };
 };
 
+//DEVELOPMENT
 // export const getServerSideProps = async (context) => {
 //   const { orderId } = context.query;
 
